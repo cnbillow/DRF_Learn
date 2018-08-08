e921725e029f1a2ef59af7db82e5b8fb6c32f5d4
admin <admin@qq.com>
2018-08-08 13:04:49 +0800

    django rest framework - serializer学习

--- serializers_learn/models.py
+++ serializers_learn/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from datetime import datetime
+from model_utils import Choices
+from model_utils.fields import StatusField, MonitorField
 
 
-class Comment(object):
+class Comment(models.Model):
+    STATUS = Choices("name", "email", "address")
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=30)
+    address = models.CharField(max_length=40)
 
-    def __init__(self, email, content, created=None):
-        self.email = email
-        self.content = content
-        self.created = created or datetime.now()
-
-
-# Create your models here.
+    status = StatusField()
+    published_at = MonitorField(monitor='status', when=['name', "email", "address"])
